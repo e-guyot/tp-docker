550e5ed7c9731da58b3b01ab453cf7f71ac285dc
e-guyot <56032211+e-guyot@users.noreply.github.com>
2019-10-16 21:27:54 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>tp-docker</title>
+  </head>
+  <body>
+    <h1>Bonjour</h1>
+    <p>Ceci est le tp-docker</p>
+  </body>
+</html>
